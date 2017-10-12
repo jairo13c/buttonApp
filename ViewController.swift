94aa4ec2c1d85527a15c974c381c19dacfc5f39a
jairo13c <31705891+jairo13c@users.noreply.github.com>
2017-10-12 12:33:34 -0600

    i declared a if statment

---
+++ ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  buttonApp
+//
+//  Created by Cortez, Jairo on 10/10/17.
+//  Copyright © 2017 Cortez, Jairo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var firstButton: UIButton!
+
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        firstButton.backgroundColor =
+            createRandomColor()
+        view.backgroundColor = createRandomColor()
+        
+        firstButton.setTitleColor(createRandomColor(), for: .normal)
+    }
+    private func createRandomColor()-> UIColor
+    {
+        //need red green and blue vaue CGFloat
+        let newColor : UIColor
+        let redValue = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let greenValue = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        let blueValue = CGFloat (Double (arc4random_uniform(256)) / 255.00)
+        
+        newColor = UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: CGFloat(1.0))
+        return newColor
+    }
+    
+    
+    override func viewDidLoad()
+    {
+        if(firstButton.backgroundColor == .blue)
+        {
+            (firstButton.backgroundColor = .red)
+        }
+        else
+        {
+            firstButton.backgroundColor = .blue
+        }
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        }
+        firstButton.fontStyle =
+            createRandomFontStyle
+        view.backgroundFontStyle = createRandomFontStyle
+    
+    }
+    private func createRandomeFontSize()-> UIFontTextStyle
+    {
+        //adding font size of text
+        let newFontSize : UIFontTextStyle
+        let ValuenewFontSize =
+
+
+    override func didReceiveMemoryWarning()
+    {
+    
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+     }
+
+
+}
+
